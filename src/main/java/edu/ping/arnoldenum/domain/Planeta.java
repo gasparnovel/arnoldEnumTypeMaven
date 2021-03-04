21f55db7e0897687250094cd0b264d03f4b6ed18
gasparnovel <gnovel@cifpfbmoll.eu>
2021-03-04 13:32:33 +0100

    feat: :sparkles: Importación de los Test del tutor

--- src/main/java/edu/ping/arnoldenum/domain/Planeta.java
+++ src/main/java/edu/ping/arnoldenum/domain/Planeta.java
@@ -1,5 +1,19 @@
 package edu.ping.arnoldenum.domain;
 
 public class Planeta {
+
+	public static final String MERCURY = null;
+    public static final String JUPITER = null;
+	public static final String NEPTUNE = null;
+
+    public static String[] planetas;
+
+    public Planeta(String[] planetas) {
+        this.planetas = planetas;
+    }
+
+    
+
+
     
 }
